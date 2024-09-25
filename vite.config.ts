c22eb23afbef8c8a448631fac24085b2e5e758bd
Shane Richards <shane.richards121@gmail.com>
2024-09-25 01:41:55 -0400

    chore: fixing base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/morse-in-code/',
   plugins: [react()],
   resolve: {
     alias: {
