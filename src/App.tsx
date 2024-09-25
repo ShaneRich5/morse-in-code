4fa6288384819f261fe5535f137b1ba667c2e108
Shane Richards <shane.richards121@gmail.com>
2024-09-25 00:15:31 -0400

    feat: add logic to convert string to morse code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { useState } from 'react';
+import { convertTextToMorseCode } from './lib/utils';
 
 const App = () => {
   const [message, setMessage] = useState('');
 
   return (
-    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 my-8'>
-      <div className='mx-auto max-w-3xl'>
-        <div className='grid w-full gap-2'>
-          <Textarea placeholder='Type your message here.' />
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 my-8">
+      <div className="mx-auto max-w-3xl">
+        <div className="grid w-full gap-2">
+          <Textarea
+            placeholder="Type your message here."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
           <Button>Send message</Button>
         </div>
+        <div className='mt-4'>Output: {convertTextToMorseCode(message)}</div>
       </div>
     </div>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { MAP_OF_LETTER_TO_MORSE_CODE } from "./constants"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const convertTextToMorseCode = (text: string) => text
+    .toLowerCase()
+    .split('')
+    .map((letter) => MAP_OF_LETTER_TO_MORSE_CODE[letter] || letter)
+    .join(' ')
