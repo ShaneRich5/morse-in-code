b129b8319985ea7b82578401549e3790d13738be
Shane Richards <shane.richards121@gmail.com>
2024-09-25 00:06:49 -0400

    chore: configuring prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import { useState } from 'react';
 
 const App = () => {
+  const [message, setMessage] = useState('');
+
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 my-8">
-      <div className="mx-auto max-w-3xl">
-        <div className="grid w-full gap-2">
-          <Textarea placeholder="Type your message here." />
+    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 my-8'>
+      <div className='mx-auto max-w-3xl'>
+        <div className='grid w-full gap-2'>
+          <Textarea placeholder='Type your message here.' />
           <Button>Send message</Button>
         </div>
       </div>
