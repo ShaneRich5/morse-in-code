ef50ed1559a4a0754c62c21b5e9f78d36017ffc7
Shane Richards <shane.richards121@gmail.com>
2024-09-25 00:25:59 -0400

    feat: ability to copy to clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { useState } from 'react';
-import { convertTextToMorseCode } from './lib/utils';
+import { convertTextToMorseCode } from '@/lib/utils';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 
 const App = () => {
   const [message, setMessage] = useState('');
+  const morseCode = convertTextToMorseCode(message)
 
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 my-8">
@@ -15,9 +25,21 @@ const App = () => {
             onChange={(e) => setMessage(e.target.value)}
             value={message}
           />
-          <Button>Send message</Button>
+          <Button>Save message</Button>
         </div>
-        <div className='mt-4'>Output: {convertTextToMorseCode(message)}</div>
+
+        <Card className='mt-4'>
+          <CardHeader>
+            <CardTitle>Output</CardTitle>
+            <CardDescription>Live translations</CardDescription>
+          </CardHeader>
+          <CardContent>
+            {morseCode ?? 'No message'}
+          </CardContent>
+          <CardFooter>
+            <Button onClick={() => navigator.clipboard.writeText(morseCode)}>Copy to Clipboard</Button>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
